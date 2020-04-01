508195cf40bbecad7ea636dcb9cf04bcb8a73cc6
LianaAlmazova <62878225+LianaAlmazova@users.noreply.github.com>
2020-04-01 16:44:04 +0600

    Update task6.m

--- task6.m
+++ task6.m
@@ -2,14 +2,13 @@
 //содержит одинаковые элементы, и каждая последующая - на 1 большие
 
 function[A] = task6(i,j)
-A = randi(i,j);
+A = randi(10,i,j);
 for k = 2:i
     A(k,1) = A(k-1,1) + 1;
-
 end
-for l = 1:i
-    for n = 2:j
-        A(l,n) = A(l,1);
+for l = 2:j
+    for t = 1:i
+        A(t,l) = A(t,1);
     end
 end
 end
