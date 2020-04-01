5842670d77bc30c087d56382d30bd96b117231b4
LianaAlmazova <62878225+LianaAlmazova@users.noreply.github.com>
2020-04-01 16:43:27 +0600

    Update task8.m

--- task8.m
+++ task8.m
@@ -1,12 +1,16 @@
 //Написать функцию, которая по заданному аргументу - матрице формирует новую, представляющую собой исходную, 
 //окруженную n "слоями" нулей
 
-function [A] = task8(n)
-l = length(n)+2;
+function [A] = task8(D,n)
+s = size(D);
+st = s(1,1);
+stb = s(1,2);
+l = size(D)+2*n;
 A = zeros(l);
-for i = 2:(l-1)
-    for j = 2:(l-1)
-        A(i,j) = n(i-1,j-1);
+
+for i = n+1:n+st
+    for j = n+1:n+stb
+        A(i,j) = D(i-n,j-n);
     end
 end
 end
